ee58dad839454caafbeb0b40973ab60a149a2e50
fjmiguel0704 <fernandoj.miguel@iesnervion.es>
2023-03-09 14:38:17 +0100

    Terminar y documentar

---
+++ Tema7-Ejercicios/src/ejercicio1/CuentaCorriente.java
@@ -0,0 +1,33 @@
+package ejercicio1;
+
+public class CuentaCorriente {
+	String DNI;
+	String nombre;
+	double saldo;
+
+	public CuentaCorriente(String DNI, double saldo) {
+		this.DNI = DNI;
+		this.saldo = saldo;
+	}
+
+	public CuentaCorriente(String DNI, String nombre, double saldo) {
+		this.DNI = DNI;
+		this.nombre = nombre;
+		this.saldo = saldo;
+	}
+
+	boolean sacarDinero(double cantidad) {
+		boolean resultado = false;
+		if (cantidad <= saldo) {
+			resultado = true;
+			saldo -= cantidad;
+		}
+
+		return resultado;
+	}
+	
+	double ingresarDinero(double cantidad) {
+		saldo+=cantidad;
+		return saldo;
+	}
+}
