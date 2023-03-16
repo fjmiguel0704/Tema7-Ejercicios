9d96393add8bb5f854962c6ab260f435e4966bf7
fjmiguel0704 <fernandoj.miguel@iesnervion.es>
2023-03-16 13:20:00 +0100

    Por comentar

--- Tema7-Ejercicios/src/ejercicio4/Main.java
+++ Tema7-Ejercicios/src/ejercicio4/Main.java
@@ -6,17 +6,18 @@ public class Main {
 
 	public static void main(String[] args) {
 		int descuento;
-		
-		Scanner read = new Scanner (System.in);
-		
+
+		Scanner read = new Scanner(System.in);
+
 		Articulo objeto = new Articulo("Keyboard", 5, 9);
-		
-		System.out.println("Introduce el descuento que desea aplicarle al artículo: ");
-		descuento=read.nextInt();
-		
-		objeto.getPVP();
-		objeto.getPVPDescuento(descuento);
-		System.out.println(objeto.imprime());
+
+		while (!objeto.nombre.equals(null) && objeto.precio != 0 && objeto.cuantosQuedan > 0) {
+			System.out.println("Introduce el descuento que desea aplicarle al artículo: ");
+			descuento = read.nextInt();
+			objeto.getPVP();
+			objeto.getPVPDescuento(descuento);
+			System.out.println(objeto.imprime());
+		}
 
 	}
 
