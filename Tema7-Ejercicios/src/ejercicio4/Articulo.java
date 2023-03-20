beb04f270be065ce72c306b4a2d1dc81aba538a1
fjmiguel0704 <fernandoj.miguel@iesnervion.es>
2023-03-20 09:12:26 +0100

    Completado

--- Tema7-Ejercicios/src/ejercicio4/Articulo.java
+++ Tema7-Ejercicios/src/ejercicio4/Articulo.java
@@ -7,22 +7,27 @@ public class Articulo {
 	int cuantosQuedan;
 
 	public Articulo(String nombre, double precio, int cuantosQuedan) {
-		this.nombre = nombre;
 		if (nombre == null) {
 			System.out.println("El nombre no es válido");
+		} else {
+			this.nombre = nombre;
 		}
-		this.precio = precio;
-		if (precio == 0.0) {
+		
+		if (precio <= 0.0) {
 			System.out.println("El precio no es válido");
+		} else {
+			this.precio = precio;
 		}
-		this.cuantosQuedan = cuantosQuedan;
+		
 		if (cuantosQuedan <= 0) {
 			System.out.println("La cantidad de artículos disponible no es válida");
+		} else {
+			this.cuantosQuedan = cuantosQuedan;
 		}
 	}
 
-	String imprime() {
-		String resultado;
+	public String imprime() {
+		String resultado = "";
 		resultado = "Nombre Artículo: " + nombre + "\n" + "Precio Artículo: " + precio + "€" + "\n" + "IVA: " + IVA + "%" + "\n"
 				+ "Disponibles: " + cuantosQuedan;
 		return resultado;
